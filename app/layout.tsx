8a1a2d38841619aef125302fb8dc5d911652c206
Micael Robert <micael.robert23@gmail.com>
2025-08-06 15:55:06 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -10,8 +11,21 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
   },
-  description: "Descubra os melhores lugares de Saquarema com nosso guia completo para moradores e visitantes",
-    generator: 'v0.dev'
+  openGraph: {
+    title: "ExploraSaquá - Guia de Saquarema",
+    description: "Descubra os melhores lugares de Saquarema com nosso guia completo para moradores e visitantes",
+    url: "https://explora-saqua.vercel.app/",
+    siteName: "ExploraSaquá",
+    images: [
+      {
+        url: "/logo2sq.png",
+        width: 1200,
+        height: 630,
+        alt: "ExploraSaquá Logo",
+      },
+    ],
+    type: "website",
+  },
 }
 
 export default function RootLayout({
@@ -23,5 +37,6 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className={inter.className}>{children}</body>
     </html>
+    
   )
 }
